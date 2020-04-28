470d0d51e93cae321c16ec6b239ad23cfb9d996d
Jakub Kozlowski <jakoz123@gmail.com>
2020-04-29 00:30:43 +0200

    Configure test libraries and modify App component to pass render test

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,18 @@
 import React, { Suspense, FC } from "react";
-import { Switch, Redirect, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route
+} from "react-router-dom";
+import { Normalize } from "styled-normalize";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
+import "i18n";
+import { store, persistor } from "store/index";
 import { GlobalStyle } from "styles/GlobalStyle";
+
 import ProtectedRoute from "components/ProtectedRoute";
 
 import Login from "pages/Login";
@@ -9,15 +20,22 @@ import Register from "pages/Register";
 import Dashboard from "pages/Dashboard";
 
 const App: FC = () => (
-  <Suspense fallback={<div>Loading translations... </div>}>
-    <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="/register" component={Register} />
-      <ProtectedRoute path="/app" component={Dashboard} />
-      <Redirect to="/" />
-    </Switch>
-    <GlobalStyle />
-  </Suspense>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Normalize />
+      <Router>
+        <Suspense fallback={<div>Loading translations... </div>}>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/register" component={Register} />
+            <ProtectedRoute path="/app" component={Dashboard} />
+            <Redirect to="/" />
+          </Switch>
+          <GlobalStyle />
+        </Suspense>
+      </Router>
+    </PersistGate>
+  </Provider>
 );
 
 export default App;
